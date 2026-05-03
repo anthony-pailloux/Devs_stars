c24c19509ad5062849a80d7ffd9090637cfcb74c
anthony-pailloux <anthony.pailloux@laplateforme.io>
2026-05-03 19:27:44 +0200

    first commit

--- calcul.php
+++ calcul.php
@@ -1,23 +1,50 @@
 <?php
-$a = 10;
-$b = 3;
-$c = 0;
-$result = 0;
 
 
-function calculer($a, $b, $c, $result) {
-    if ($result === 0) {
-         echo $a + $b;
-    }else{
-        echo 'Erreur : division par zéro';
-    }
+
+
+
+// function calculer($a, $b, $c, $result) {
+//     if ($result === 0) {
+//          echo $a + $b;
+//     }else{
+//         echo 'Erreur : division par zéro';
+//     }
    
+// }
+
+// calculer($a, $b, $c, $result);
+
+
+function calcul ($nombre1, $nombre2, $operateur)
+{
+    if ($operateur == '+')
+    {
+        return $nombre1 + $nombre2;
+    }
+
+    if ($operateur == '-')
+    {
+        return $nombre1 - $nombre2;
+    }
+
+    if ($operateur == '*')
+    {
+        return $nombre1 * $nombre2;
+    }
+
+    if ($operateur == '/')
+    {
+        if($nombre2 != 0)
+            {
+                return $nombre1 / $nombre2;
+            }
+        else {
+            return 'impossible de diviser par zero';
+        }
+        
+    }
 }
 
-calculer($a, $b, $c, $result);
 
-
-
-
-
-
+echo(calcul (10, 0, '/'));
